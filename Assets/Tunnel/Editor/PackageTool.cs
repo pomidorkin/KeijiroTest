1001c48fad412c4d1496f36d20ac8996a9f40caf
Pomidorkin <pbriuzgin@gmail.com>
2023-09-03 16:33:52 +0900

    Genious Keijiro

---
+++ Assets/Tunnel/Editor/PackageTool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PackageToolTwoCust
+{
+    [MenuItem("Package/Update Package")]
+    static void UpdatePackage()
+    {
+        AssetDatabase.ExportPackage("Assets/Kvant", "KvantTunnel.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
